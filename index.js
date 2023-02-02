455eb61b11516863f6069fb4ee81ccd541a038a4
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2023-02-02 23:20:02 +0530

    Created index.js file for html

---
+++ index.js
@@ -0,0 +1,7 @@
+
+for(var x = 0; x < 60; x++){
+    var divElemet = document.createElement("div");
+    divElemet.className = 'star';
+
+    document.querySelector("div.stars").appendChild(divElemet);
+}
