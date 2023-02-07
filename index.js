9db02c60749bc475578048f3c8078241d59aa81c
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2023-02-07 15:02:37 +0530

    Deleting the index,js fiel

--- index.js
+++
@@ -1,7 +0,0 @@
-
-for(var x = 0; x < 60; x++){
-    var divElemet = document.createElement("div");
-    divElemet.className = 'star';
-
-    document.querySelector("div.stars").appendChild(divElemet);
-}
