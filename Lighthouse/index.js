201dae537f4bd2593f26b0438d8cb2c3135cdf39
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2023-02-24 15:00:51 +0530

    added window

--- Lighthouse/index.js
+++ Lighthouse/index.js
@@ -21,3 +21,19 @@ for(var x = 0; x < 30; x++){
 
     document.querySelector("div.sea").appendChild(divElemet);
 }
+
+// //sails
+// for(var x = 0; x < 2; x++){
+//     var divElemet = document.createElement("div");
+//     divElemet.className = "sail";
+
+//     document.querySelector("div.boat").appendChild(divElemet);
+// }
+
+//windows
+for(var x = 0; x < 4; x++){
+    var divElemet = document.createElement("div");
+    divElemet.className = "window";
+
+    document.querySelector("div.windows").appendChild(divElemet);
+}
